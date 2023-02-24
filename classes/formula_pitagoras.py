7ba85670eab0bc426891cebb30964a1222026c43
MateusBrandeburski <mateus.brandeburski92@gmail.com>
2023-02-24 14:17:41 -0300

    Json na rota query add html que rotorna um json

--- classes/formula_pitagoras.py
+++ classes/formula_pitagoras.py
@@ -3,7 +3,6 @@ class TeoremaDePitagoras():
     
     # função construtora
     def __init__(self,  catetoA=0, catetoO=0, hipotenusa=0):
-        
         """Explicação dessa conversão
         Essas converções e condições são necessárias porque na rota são decalarados 3 tipos de query paramns, e quando um desses parametros não é passado, ele retorna um item None (e um item None não pode ser multiplicado, o que faz com que esse item caia no tratamento de erro). Então "float(catetoO) if catetoO is not None else 0" é a condição que faz com que, caso seja None, ele vire 0.
         
@@ -33,18 +32,17 @@ class TeoremaDePitagoras():
         cateto_final = passa_subtraindo ** (1/2) # ou 0,5    
         return cateto_final
       
-    def calcular_hipotenusa(self):
-        
+    def calcular_hipotenusa(self):    
         quadrado_da_hipotenusa = self.__catetoO**2 + self.__catetoA**2
         hipotenusa = quadrado_da_hipotenusa ** (1/2)
         return hipotenusa
    
-    def strings_teorema(self, name):
-             
+    @staticmethod
+    def strings_teorema(name):       
         if "hipotenusa" in name:
             name = "hipotensa"
             return name
-        else:
+        elif "cateto" in name:
             name = "cateto"
-            return name
-            
+            return name 
+        #preciso tratar o NONEddd
