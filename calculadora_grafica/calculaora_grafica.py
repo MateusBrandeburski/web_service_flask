00c1439cdd21c028a0e6b9fb0f0f09da1dce0393
MateusBrandeburski <mateus.brandeburski92@gmail.com>
2023-03-01 14:49:08 -0300

    comentários da roto calculadora_grafica

--- calculadora_grafica/calculaora_grafica.py
+++ calculadora_grafica/calculaora_grafica.py
@@ -11,6 +11,7 @@ enviar = Email(os.environ["EMAIL"], os.environ["SENHA"])
 @calculadora_grafica.route("/calculadora", methods=['GET'])
 def index():
      
+     # verifica se o parâmetro resposta, do render_templete contém alguma string.
      global resposta
      if 'resposta' in globals() and resposta != "":
           return render_template('back_html.html', resposta=resposta)
@@ -23,22 +24,22 @@ def index():
 @calculadora_grafica.route("/calculadora", methods=['POST'])
 def calculadora():
       
+     # verifica se o botão calcular foi clicado
      if request.form['action'] == 'calcular':
           
-          if  (request.form.get('hipotenusa') == "") and (request.form.get('cA') == "") and  (request.form.get('cO') == "") or (request.form.get('hipotenusa') == "") and (request.form.get('cA') == "") or  (request.form.get('hipotenusa') == "") and (request.form.get('cO') == "") or (request.form.get('cA') == "") and (request.form.get('cO') == "") or (request.form.get('cA') != "") and (request.form.get('cO') != "") and  (request.form.get('hipotenusa') != ""):
-               
+          # verifica 3 ou 2 campos foram deixados em branco, além de verificar se os 3 campos foram preenchidos.
+          if  (request.form.get('hipotenusa') == "") and (request.form.get('cA') == "") and  (request.form.get('cO') == "") or (request.form.get('hipotenusa') == "") and (request.form.get('cA') == "") or  (request.form.get('hipotenusa') == "") and (request.form.get('cO') == "") or (request.form.get('cA') == "") and (request.form.get('cO') == "") or (request.form.get('cA') != "") and (request.form.get('cO') != "") and  (request.form.get('hipotenusa') != ""):   
+                  
                    global resposta
                    resposta = TeoremaDePitagoras.strings_teorema(name="falta_parametros")
                    return redirect(url_for('calculadora_grafica.index'))
           
-          
+          # Se 2 campos forem preenchidos corretamente:
           else:
-                          
-               if request.form.get('hipotenusa') != "":
-                    """
-                    Passando os 3 parâmetros, porque ele receber qualquer um dos lados do cateto para calcular o outro lado.
-                    """
-                    
+                              
+               # calcula os catetos
+               if request.form.get('hipotenusa') != "":   
+                    # passando os 3 parâmetros, porque ele receber qualquer um dos lados do cateto para calcular o outro lado.
                     teorema = TeoremaDePitagoras(catetoA=request.form.get('cA'),
                                              catetoO=request.form.get('cO'), 
                                              hipotenusa=request.form.get('hipotenusa'))
@@ -47,9 +48,8 @@ def calculadora():
                     resposta = round(teorema.calcular_catetos(), 2)
                     return redirect(url_for('calculadora_grafica.index'))
      
-                    
-               elif request.form.get('hipotenusa') == "":
-                    
+               # calcula a hipotenusa
+               elif request.form.get('hipotenusa') == "":   
                     teorema = TeoremaDePitagoras(catetoA=request.form.get('cA'), catetoO=request.form.get('cO'))
                     resposta = round(teorema.calcular_hipotenusa(), 2)
                     return redirect(url_for('calculadora_grafica.index'))
@@ -57,4 +57,3 @@ def calculadora():
 
                
           
-         # erros para tratar. Número negativo, 1 campo preenchido, 3 campos prenchido, 3 campos não preenchidos
