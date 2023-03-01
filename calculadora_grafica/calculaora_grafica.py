95a3b6e72a73c44fe7ad71372c2c8dca8b29dc2d
MateusBrandeburski <mateus.brandeburski92@gmail.com>
2023-03-01 13:29:01 -0300

    Caluladora de Pitágoras - Rota com Gráfica Tramento de erro 0 campos preenchidos 2 campos sem preencher 1 campos sem preencher

--- calculadora_grafica/calculaora_grafica.py
+++ calculadora_grafica/calculaora_grafica.py
@@ -7,7 +7,7 @@ calculadora_grafica = Blueprint('calculadora_grafica', __name__, template_folder
 enviar = Email(os.environ["EMAIL"], os.environ["SENHA"])
 
 
-# Rota index para protrocolo HTTPS GET
+# Rota index para protrocolo HTTPS GET. Ou seja, é a rota que renderiza a calculadora na tela.
 @calculadora_grafica.route("/calculadora", methods=['GET'])
 def index():
      
@@ -18,32 +18,60 @@ def index():
      else:
           return render_template('back_html.html')
      
-
-
-
-
+     
+# Rota POST que serve apenas as ações da Calculadora de Pitágoras 
 @calculadora_grafica.route("/calculadora", methods=['POST'])
 def calculadora():
       
      if request.form['action'] == 'calcular':
           
-          if request.form.get('hipotenusa') != "":
-               """
-               Passando os 3 parâmetros, porque ele receber qualquer um dos lados do cateto para calcular o outro lado.
-               """
-               teorema = TeoremaDePitagoras(catetoA=request.form.get('cA'),
-                                            catetoO=request.form.get('cO'), 
-                                            hipotenusa=request.form.get('hipotenusa'))
+          if  (request.form.get('hipotenusa') == "") and (request.form.get('cA') == "") and  (request.form.get('cO') == ""):
+               
+                   global resposta
+                   resposta = TeoremaDePitagoras.strings_teorema(name="falta_parametros")
+                   return redirect(url_for('calculadora_grafica.index'))
+          
+          elif (request.form.get('hipotenusa') == "") and (request.form.get('cA') == ""):
+               
+             
+               resposta = TeoremaDePitagoras.strings_teorema(name="falta_parametros")
+               return redirect(url_for('calculadora_grafica.index'))
               
-               global resposta
-               resposta = round(teorema.calcular_catetos(), 2)
+          elif (request.form.get('hipotenusa') == "") and (request.form.get('cO') == ""):
+                              
+   
+               resposta = TeoremaDePitagoras.strings_teorema(name="falta_parametros")
                return redirect(url_for('calculadora_grafica.index'))
-  
-               
-          elif request.form.get('hipotenusa') == "":
-               
-               teorema = TeoremaDePitagoras(catetoA=request.form.get('cA'), catetoO=request.form.get('cO'))
-               resposta = teorema.calcular_hipotenusa()
+          
+          elif (request.form.get('cA') == "") and (request.form.get('cO') == ""):
+              
+               resposta = TeoremaDePitagoras.strings_teorema(name="falta_parametros")
                return redirect(url_for('calculadora_grafica.index'))
+          
+           
+          else:
+                          
+               if request.form.get('hipotenusa') != "":
+                    """
+                    Passando os 3 parâmetros, porque ele receber qualquer um dos lados do cateto para calcular o outro lado.
+                    """
+                    
+                    teorema = TeoremaDePitagoras(catetoA=request.form.get('cA'),
+                                             catetoO=request.form.get('cO'), 
+                                             hipotenusa=request.form.get('hipotenusa'))
+                    
+                    
+                    resposta = round(teorema.calcular_catetos(), 2)
+                    return redirect(url_for('calculadora_grafica.index'))
+     
+                    
+               elif request.form.get('hipotenusa') == "":
+                    
+                    teorema = TeoremaDePitagoras(catetoA=request.form.get('cA'), catetoO=request.form.get('cO'))
+                    resposta = round(teorema.calcular_hipotenusa(), 2)
+                    return redirect(url_for('calculadora_grafica.index'))
+               
+
+               
           
          # erros para tratar. Número negativo, 1 campo preenchido, 3 campos prenchido, 3 campos não preenchidos
--- classes/formula_pitagoras.py
+++ classes/formula_pitagoras.py
@@ -2,18 +2,19 @@
 
 class TeoremaDePitagoras():
     
-    # função construtora
     def __init__(self,  catetoA=0, catetoO=0, hipotenusa=0):
         """Explicação dessa conversão
         Essas converções e condições são necessárias porque na rota são decalarados 3 tipos de query paramns, e quando um desses parametros não é passado, ele retorna um item None (e um item None não pode ser multiplicado, o que faz com que esse item caia no tratamento de erro). Então "float(catetoO) if catetoO is not None else 0" é a condição que faz com que, caso seja None, ele vire 0.
         
         Acima, em self, precisa ser passado como (catetoA=0, catetoO=0, hipotenusa=0) para que não precise sempre declarar todos sempre que for usar a classe, por exemplo: teorema=TeoremaDePitagoras(catetoA=catetoA, catetoO=catetoO).
+        
+        O if ternário trata tanto None quando uma string vázia (""), porque em query, se não for passado nada é None, mas em um input html, se não for passado nada é "".
+        
+        Receber o valor 0 (zero)  dá certo porque a fórmula começa somando = cO²+cA² = h²
+        Se começasse multiplicando, ele teria que receber como valor 1 (um)
         """
-        self.__catetoO = catetoO
         self.__catetoO= float(catetoO) if catetoO is not "" and catetoO is not None else 0
-        self.__catetoA = catetoA
         self.__catetoA = float(catetoA) if catetoA is not "" and catetoA is not None else 0
-        self.__hipotenusa = hipotenusa
         self.__hipotenusa = float(hipotenusa) if hipotenusa is not "" is not None else 0
         
     def calcular_catetos(self):
@@ -44,7 +45,9 @@ class TeoremaDePitagoras():
             return name
         elif "cateto" in name:
             return name 
+        elif "falta_parametros" in name:
+            name = "Dois campus devem ser preenchidos para calcular: 'se tivermos 2 lados de um triângilo retângulo podemos calcular o outro lado.'"
+            return name
         
 
-        
-       
+
