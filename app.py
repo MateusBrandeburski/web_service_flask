859684e441a4866d0a6e99ae726b2a5adf0a0036
MateusBrandeburski <mateus.brandeburski92@gmail.com>
2023-03-10 14:02:30 -0300

    alterções nas pastas

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
-from flask import Flask, Blueprint, url_for
-from calculadora_grafica.calculaora_grafica import calculadora_grafica
-from calculadora_query.calculadora_query import calcularora_query
-from limpa_email.limpa_email import limpa_email
-import asyncio
+from flask import Flask
+from blueprint_calculadora_grafica.calculaora_grafica import calculadora_grafica
+from blueprint_calculadora_query.calculadora_query import calcularora_query
+from blueprint_limpa_email.limpa_email import limpa_email
+from blueprint_keyclock.keyclock import keyclock
+
 
 app = Flask(__name__)
+
 app.register_blueprint(calculadora_grafica)
 app.register_blueprint(calcularora_query)
 app.register_blueprint(limpa_email)
 
 
 if __name__ == '__main__':
-    asyncio.get_event_loop()
     app.run( host='0.0.0.0', port=8000, debug=True)
