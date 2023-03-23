479be63e2e4792c4eeec47a3d0df3f85c3bf14da
MateusBrandeburski <mateus.brandeburski92@gmail.com>
2023-03-23 13:21:24 -0300

    botão Atulaizar funcionando Agora a aba 'rpa' é renderizada vazia e o scraping só é feito após apertar o botão atualizar

--- blueprints/RPA/rpa.py
+++ blueprints/RPA/rpa.py
@@ -7,23 +7,29 @@ rpa = Blueprint('rpa', __name__, template_folder='templates')
 
 @rpa.route('/rpa')
 def index():
+    
     if 'usuario_logado' not in session or session['usuario_logado'] == None:
-        
-          return redirect(url_for('login.index'))
+        return redirect(url_for('login.index'))
       
-    else:
-        
+    else:      
+        return render_template('rpa/rpa.html')
+
+
+@rpa.route('/minera-dados')
+def minera_dados():
+    
         scraping1 = WebScrapingBS4(link="https://spacetoday.com.br/")
-        scraping1_noticias = scraping1.perga_texto(elementoFILHO='a', elementoPAI='h2', tipoPAI='class', descricaoPAI='entry-title h2')
+        noticias = scraping1.perga_texto(elementoFILHO='a', elementoPAI='h2', tipoPAI='class', descricaoPAI='entry-title h2')
+        links = scraping1.pega_url(elementoFILHO='a', elementoPAI='h2', tipoPAI='class', descricaoPAI='entry-title h2')
+        
+        itens_scraping1=zip(noticias, links)
 
 
         scraping2 = WebScrapingBS4(link='https://anmtv.com.br/')
         noticias = scraping2.perga_texto(elementoPAI='h3', tipoPAI='class', descricaoPAI='post__title typescale-2_5 line-limit-child line-limit-3', elementoFILHO='a')
         links = scraping2.pega_url(elementoPAI='h3', tipoPAI='class', descricaoPAI='post__title typescale-2_5 line-limit-child line-limit-3', elementoFILHO='a')
         
-        
         itens_scraping2=zip(noticias, links)
         
-        return render_template('rpa/rpa.html', scraping1=scraping1_noticias, itens_scraping2=itens_scraping2)
-
-
+        return render_template('rpa/rpa.html',  itens_scraping1=itens_scraping1, itens_scraping2=itens_scraping2)
+        
