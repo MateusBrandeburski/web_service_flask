e62937584f41c53c63986c7c564397791a8913e0
MateusBrandeburski <mateus.brandeburski92@gmail.com>
2023-04-06 11:50:34 -0300

    atualizações nas rotas e no DB

--- blueprints/api/crud/crud.py
+++ blueprints/api/crud/crud.py
@@ -3,46 +3,55 @@ from .database import db
 
 crud = Blueprint('crud', __name__, template_folder='template')
 
-class Estudantes(db.Model):
+class Endereco(db.Model):
     id = db.Column('id', db.Integer, primary_key=True, autoincrement=True)
     nome = db.Column(db.String(150))
-    idade = db.Column(db.Integer)
+    email = db.Column(db.String(150))
+    cidade = db.Column(db.String(20))
+
     
-    def __init__(self, nome, idade):
+    def __init__(self, nome, email, cidade):
         self.nome = nome
-        self.idade = idade
+        self.email = email
+        self.cidade = cidade
+
+        
          
 @crud.route('/crud')
 def index():
-    estudantes = Estudantes.query.all()
-    return render_template('api/crud/tabela.html', estudantes=estudantes)
+    endereco = Endereco.query.all()
+    return render_template('api/crud/tabela.html', endereco=endereco)
      
 
 @crud.route('/add', methods=['GET','POST'])
 def add():   
     if request.method == 'POST':
-        estudante = Estudantes(request.form['nome'], request.form['idade'])
-        db.session.add(estudante) #salva no DB
+        
+        endereco = Endereco(request.form['nome'], request.form['email'], request.form['cidade'])
+        print(endereco)
+        db.session.add(endereco) #salva no DB
         db.session.commit()
+        
         return redirect(url_for('crud.index'))
     return render_template('api/crud/add.html')
 
 
 @crud.route('/edit/<int:id>', methods=['GET','POST'])
 def edit(id):
-    estudante = Estudantes.query.get(id)
+    endereco = Endereco.query.get(id)
     if request.method == 'POST':
-        estudante.nome = request.form['nome']
-        estudante.idade = request.form['idade']
+        endereco.nome = request.form['nome']
+        endereco.email = request.form['email']
+        endereco.cidade = request.form['cidade']
         db.session.commit()
         return redirect(url_for('crud.index'))
-    return render_template('api/crud/edit.html', estudante=estudante) 
+    return render_template('api/crud/edit.html', endereco=endereco) 
         
 
 @crud.route('/delete/<int:id>')
 def delete(id):
-    estudante = Estudantes.query.get(id)
-    db.session.delete(estudante)
+    endereco = Endereco.query.get(id)
+    db.session.delete(endereco)
     db.session.commit()
     return redirect(url_for('crud.index'))
 
