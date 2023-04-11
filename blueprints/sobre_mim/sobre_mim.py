0d08a1cb4a10c5d540538ef3a41354d811a21ff1
MateusBrandeburski <mateus.brandeburski92@gmail.com>
2023-04-11 20:48:16 -0300

    troca de template do home

--- blueprints/sobre_mim/sobre_mim.py
+++ blueprints/sobre_mim/sobre_mim.py
@@ -2,6 +2,6 @@ from flask import Blueprint, render_template, redirect, session, url_for
 
 sobre = Blueprint('sobre', __name__, template_folder='template')
 
-@sobre.route('/sobre-mim')
+@sobre.route('/sobre-o-projeto')
 def index():
-    return render_template('sobre_mim/sobre_mim.html')
+    return render_template('sobre_o_projeto/sobre_o_projeto.html')
