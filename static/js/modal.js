46b415f96dc0e9a30ba6ac4c6ed45f31eb8719ae
MateusBrandeburski <mateus.brandeburski92@gmail.com>
2023-04-14 11:16:53 -0300

    sibar fecha quando clicar em qualquer lugar da página

--- static/js/modal.js
+++ static/js/modal.js
@@ -1,11 +1,14 @@
 
-function closeNav() {
-    document.querySelector('.navbar-collapse').classList.remove('show');
-  }
-  
 $(document).ready(function(){
     $('[data-bs-toggle="tooltip"]').tooltip();  // ativa o tooltip
     $(".abre-sidebar").hover(function(){     // exibe a sidebar quando passar o mouse sobre o elemento que contém a classe "abre-sidebar"
       $("#exampleModal").modal("show");
     });
+    
+    $("body").click(function(event){    // fecha a sidebar quando clicar em qualquer lugar da página
+        var target = $(event.target);
+        if(!target.closest("#exampleModal").length && !target.hasClass("abre-sidebar")) {
+          $("#exampleModal").modal("hide");
+        }
+      });
   });
