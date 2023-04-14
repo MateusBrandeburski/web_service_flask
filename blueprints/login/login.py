a0adeb6307f252f56d462f97371e0b951e1f7633
MateusBrandeburski <mateus.brandeburski92@gmail.com>
2023-04-14 11:34:16 -0300

    redirect para o web_scraping

--- blueprints/login/login.py
+++ blueprints/login/login.py
@@ -11,7 +11,7 @@ def index():
     if 'usuario_logado' not in session or session['usuario_logado'] == None:
         return render_template('login/login.html')  
      
-    return render_template('home/home.html')
+    return render_template('web_scraping/web_scraping.html')
  
    
 @login.route('/logout')
@@ -25,7 +25,7 @@ def logout():
 def autenticar():
     if 'developer' in request.form['senha'] and 'MateusTI' in request.form['usuario']:
         session['usuario_logado'] = request.form['usuario']
-        return redirect(url_for('home.index'))
+        return redirect(url_for('web_scraping.index'))
     else:
         flash('Username ou password inválidos')
         return redirect(url_for('login.index'))
