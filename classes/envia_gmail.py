081907666998a4875efb7d13f50021b281e8e444
MateusBrandeburski <mateus.brandeburski92@gmail.com>
2023-04-26 10:16:08 -0300

    hash de senha add

--- classes/envia_gmail.py
+++ classes/envia_gmail.py
@@ -1,32 +1,3 @@
-# from validate_email import validate_email
-# from email.message import EmailMessage
-# import os
-# import smtplib
-
-# class Email():  
-    
-#     def envia_email(email_cadastrado, mensagem='Sua conta no Web Service Hub © foi criada com sucesso!'):
-#         # configurar email, senha
-#         EMAIL_ADDRESS = os.environ['EMAIL']
-#         EMAIL_PASSWORD = os.environ['SENHA']
-        
-
-#         msg = EmailMessage()
-#         msg['Subject'] = 'Web Service Hub ©'
-#         msg['From'] = EMAIL_ADDRESS
-#         msg['To'] = 'mateus.brandeburski92@gmail.com', email_cadastrado
-#         msg.set_content(mensagem)
-
-
-#         # envia o email
-#         with smtplib.SMTP_SSL('smtp.gmail.com',465) as smtp:
-#             smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
-#             smtp.send_message(msg)
-            
-#     def valida_email(email_cadastro):
-#         is_valid = validate_email(email_cadastro)
-        
-
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
@@ -84,7 +55,3 @@ class Email:
             smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
             smtp.sendmail(EMAIL_ADDRESS, email_cadastrado, msg.as_string())
 
-
-
-
-Email.envia_email('mateus.brandeburski92@gmail.com', senha="D3v3lo034r@")
